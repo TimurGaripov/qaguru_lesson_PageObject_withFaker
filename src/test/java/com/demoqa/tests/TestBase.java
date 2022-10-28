9f699a541c06f49b4607aef4b084dec570e435a1
TimurGaripov <garipov.timur@gmail.com>
2022-10-28 15:18:15 +0500

    page_objects with Faker test_jenkins_params fix

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -9,32 +9,37 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
+
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        if (System.getProperty("remote") != null) {
-            Configuration.remote = System.getProperty("remote");
-
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
+        if (System.getProperty("selenide_remote") != null) {
+            Configuration.remote = System.getProperty("selenide_remote");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+        }
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser_name", "Chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "106.0");
-        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
 
-      //  Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = System.getProperty("browser_name", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "105.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
     }
 
     @AfterEach
     void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
+        Attach.screenshotAs("screenshot");
         Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
+
+        if (Configuration.browser.equals("chrome")) {
+            Attach.browserConsoleLogs();
+        }
+        if (System.getProperty("selenide.remote") != null) {
+            Attach.addVideo();
+        }
     }
 }
